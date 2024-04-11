88dca22cdf9df8eae9d70e11f59602301aee8c10
Romeo Frouin <romeo.frouin@etu.uca.fr>
2024-04-11 11:41:45 +0200

    sql base

--- index.js
+++ index.js
@@ -7,3 +7,39 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'rfrouin',
+    password: 'Drfgthju63-',
+    database: 'rfrouin',  
+});
+
+connection.connect((err) => {
+    if(err){
+        console.error("Erreur de connexion à la BDD: " + err);
+        return;
+    }
+    console.log("Connexion réussie avec la bdd")
+});
+
+connection.query("SELECT * FROM etudiant", (err, results) => {
+    if(err){
+        console.error('Erreur lors de la récupération de la BDD : ', err)
+        return;
+    }
+    console.log("Les données sont : ", results)
+})
+/*
+app.get('/', (req, res) => {
+    connection.query('SELECT * FROM etudiant', (err, results) => {
+        if (err) {
+            console.error('Erreur lors de la récupération des données :', err);
+            res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+            return;
+        }
+        res.json(results);
+    });
+});*/
